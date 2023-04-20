4971dad2b88e1399fc06dc5326548080185f1891
Nelson Aghemo <nelsonfaghemo@gmail.com>
2023-04-20 13:17:49 -0300

    creamos react con tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}", //esto también es una forma de englobar subdirectorios
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
